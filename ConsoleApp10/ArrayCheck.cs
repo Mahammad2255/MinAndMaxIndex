18b071336f766699c4ad10d93a4f3d534bbf7c04
Mahammad2255 <[mahammadei@code.edu.az]>
2021-12-11 04:38:47 -0800

    Add project files.

---
+++ ConsoleApp10/ArrayCheck.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class ArrayCheck
+    {
+        public void Check(int []arr)
+        {
+            
+            int min;
+            int max;
+
+            min = arr[0];
+            max = arr[0];
+            int maxIndex = 0;
+            int minIndex = 0;
+           
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (min > arr[i])
+                {
+                    min = arr[i];
+                    minIndex = i;
+                    
+                }
+                if (max < arr[i])
+                {
+                    max = arr[i];
+                    maxIndex = i;
+                    
+                }
+                
+
+            }
+            Console.WriteLine("En boyuk eded: " + max + " En boyuk ededin indeksi: " + maxIndex);
+            Console.WriteLine("En kicik eded: " + min+ " En kicik ededin indeksi: " +  minIndex);
+            
+            
+        }
+       
+    }
+}
---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayCheck arraycheck = new ArrayCheck();
+
+            arraycheck.Check(new int []  { 1,8,-2,48,9});
+
+
+                
+        }
+    }
+}
